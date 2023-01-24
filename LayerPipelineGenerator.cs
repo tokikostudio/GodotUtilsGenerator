3fa5266b919c2c9b93856a5fb5d51ca17f953020
Louis Schnellbach <Xipiryon@users.noreply.github.com>
2023-01-24 22:56:40 +0100

    Improved Layers generation, now exposes Godot class extensions

--- LayerPipelineGenerator.cs
+++ LayerPipelineGenerator.cs
@@ -8,33 +8,83 @@ namespace Tokiko.SourceGenerators;
 
 public static class LayerPipelineGenerator
 {
-    private const string OutputClassName = "LayerName";
+    private const string OutputClassName = "Layer";
     private const string SectionName     = "layer_names";
 
     public static void Setup(IncrementalGeneratorInitializationContext context, IncrementalValuesProvider<ImmutableArray<SectionData>> sectionsProvider)
         => context.RegisterSourceOutput(sectionsProvider, ParseInputSection);
 
+    private class LayerPair
+    {
+        public LayerPair(string name, int shift)
+        {
+            Name  = name;
+            Shift = shift;
+        }
+
+        public readonly string Name;
+        public readonly int    Shift;
+    }
+
     private class LayerData
     {
-        public LayerData(string dimension) => _dimension = dimension;
+        public LayerData(string dimension) => Dimension = dimension;
 
-        private readonly string       _dimension;
-        public readonly  List<string> Render     = new();
-        public readonly  List<string> Physics    = new();
-        public readonly  List<string> Navigation = new();
+        public readonly string          Dimension;
+        public readonly List<LayerPair> Render     = new();
+        public readonly List<LayerPair> Physics    = new();
+        public readonly List<LayerPair> Navigation = new();
 
-        public override string ToString() => $@"    public static class Render{_dimension}
+        // container.Add(
+        private ImmutableArray<string> TransformPairsToMask(List<LayerPair> layerPairs)
+        {
+            return layerPairs.Select(lp => $"        public static uint {lp.Name} = 1 << {lp.Shift};")
+                             .ToImmutableArray();
+        }
+
+        public string GenerateMaskConstants() => $@"    public static class Render{Dimension}
     {{
-{string.Join("\n", Render)}
+{string.Join("\n", TransformPairsToMask(Render))}
     }}
-    public static class Physics{_dimension}
+    public static class Physics{Dimension}
     {{
-{string.Join("\n", Physics)}
+{string.Join("\n", TransformPairsToMask(Physics))}
     }}
-    public static class Navigation{_dimension}
+    public static class Navigation{Dimension}
     {{
-{string.Join("\n", Navigation)}
+{string.Join("\n", TransformPairsToMask(Navigation))}
     }}";
+
+        public void GenerateGodotClassExtensions(SourceProductionContext spc)
+        {
+            OutputGodotExtensionClass(spc, $"CollisionObject{Dimension}", "Physics", Physics, "CollisionLayer", "CollisionMask");
+            OutputGodotExtensionClass(spc, $"VisualInstance{Dimension}", "Render", Render, "Layers");
+            OutputGodotExtensionClass(spc, $"NavigationLink{Dimension}", "Navigation", Navigation, "NavigationLayers");
+            OutputGodotExtensionClass(spc, $"NavigationAgent{Dimension}", "Navigation", Navigation, "NavigationLayers");
+            OutputGodotExtensionClass(spc, $"NavigationRegion{Dimension}", "Navigation", Navigation, "NavigationLayers");
+        }
+
+        private void OutputGodotExtensionClass(SourceProductionContext spc,
+                                               string                  className,
+                                               string                  layerName,
+                                               List<LayerPair>         layerPairs,
+                                               params string[]         nodeFieldNames)
+        {
+            var layerPrefix     = $"{OutputClassName}.{layerName}{Dimension}";
+            var classExtensions = $"{className}Extensions";
+            var hasMethods      = new List<string>();
+            foreach (var name in nodeFieldNames)
+            foreach (var layerPair in layerPairs)
+                hasMethods.Add($"    public static bool Has{name}{layerPair.Name}(this {className} node) => (node.{name} & {layerPrefix}.{layerPair.Name}) != 0;");
+
+            var src = $@"using Godot;
+using {GodotUtilsGenerator.OutputNamespace};
+public static class {classExtensions}
+{{
+{string.Join("\n", hasMethods)}
+}}";
+            spc.AddSource($"{classExtensions}.g.cs", src);
+        }
     }
 
     private static void ParseInputSection(SourceProductionContext spc, ImmutableArray<SectionData> sections)
@@ -67,18 +117,21 @@ public static class LayerPipelineGenerator
             if (container == null)
                 continue;
 
-            var offset = int.Parse(groups["offset"].Value) - 1;
-            var name   = groups["name"].Value.SnakeToPascalCase();
+            var shift = int.Parse(groups["offset"].Value) - 1;
+            var name  = groups["name"].Value.SnakeToPascalCase();
 
-            container.Add($"        public static int {name} = 1 << {offset};");
+            container.Add(new LayerPair(name, shift));
         }
 
         var src = $@"namespace {GodotUtilsGenerator.OutputNamespace};
 public static class {OutputClassName}
 {{
-{layer2d}
-{layer3d}
+{layer2d.GenerateMaskConstants()}
+{layer3d.GenerateMaskConstants()}
 }}";
         spc.AddSource($"{OutputClassName}.g.cs", src);
+
+        layer2d.GenerateGodotClassExtensions(spc);
+        layer3d.GenerateGodotClassExtensions(spc);
     }
 }
